83271f037d2baa37b356aeade8dc80974de1fe97
kojiro629 <kojiro629@gmail.com>
2021-06-15 23:11:58 +0900

    Add test pattern to 'IchimatsuDrawerTest.cs'

--- CSharpStudyTest/Image/IchimatsuDrawerTest.cs
+++ CSharpStudyTest/Image/IchimatsuDrawerTest.cs
@@ -8,13 +8,28 @@ namespace CSharpStudyTest.Image
     [TestClass]
     public class IchimatsuDrawerTest
     {
-        private const string BITMAP_PATH = @"C:\Users\NKOJIMA\source\repos\CSharpStudy\CSharpStudy\Image\ichimatsu.png";
+        private const string BITMAP_PATH1 = @"C:\Users\NKOJIMA\source\repos\CSharpStudy\CSharpStudy\Image\ichimatsu1.png";
 
+        private const string BITMAP_PATH2 = @"C:\Users\NKOJIMA\source\repos\CSharpStudy\CSharpStudy\Image\ichimatsu2.png";
+
+        /// <summary>
+        /// キャンバスサイズが格子サイズで割り切れるパターン。
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
             IchimatsuDrawer drawer = new IchimatsuDrawer();
-            drawer.Draw(BITMAP_PATH, Brushes.DarkGreen, Brushes.Black);
+            drawer.Draw(BITMAP_PATH1, Brushes.DarkGreen, Brushes.Black);
+        }
+
+        /// <summary>
+        /// キャンバスサイズが格子サイズで割り切れないパターン。
+        /// </summary>
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IchimatsuDrawer drawer = new IchimatsuDrawer(310, 210);
+            drawer.Draw(BITMAP_PATH2, Brushes.DarkGreen, Brushes.Black);
         }
     }
 }
