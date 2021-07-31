dc8bde558e3f756824b0d17e270c864d28e3d2d2
kojiro629 <kojiro629@gmail.com>
2021-07-31 21:39:59 +0900

    Add TreeDrawerTest.cs #6

---
+++ CSharpStudyTest/TreeDrawerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CSharpStudy.Image;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpStudyTest
+{
+    [TestClass]
+    public class TreeDrawerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TreeDrawer td = new TreeDrawer();
+            td.DrawTree(500);
+        }
+    }
+}
